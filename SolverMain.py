8ee93b437a3731b09e89d266e455021257212e8e
RyanYovichin <yovichin.ryan@gmail.com>
2025-03-03 10:43:28 -0800

    starting the solver

--- SolverMain.py
+++ SolverMain.py
@@ -1,16 +1,28 @@
 import random
 from fdrawhand import drawhand
 from fdictsearch import dictwrite,elimwords
+from fgamecreate import *
+from fFixDictionary import fixdict
 hand = []
 bag = ["A","A","A","A","A","A","A","A","A","B","B","C","C","D","D","D","D","E","E","E","E","E","E","E","E","E","E","E","E","F","F","G","G","G","H","H","I","I","I","I","I","I","I","I","I","J","K","L","L","L","L","M","M","N","N","N","N","N","N","O","O","O","O","O","O","O","O","P","P","Q","R","R","R","R","R","R","S","S","S","S","T","T","T","T","T","T","U","U","U","U","V","V","W","W","X","Y","Y","Z","*","*"]
-tilepoints = {'E': 1, 'A': 1, 'I': 1, 'O': 1, 'N': 1, 'R': 1, 'T': 1, 'L': 1, 'S': 1, 'U': 1, 'D': 2, 'G': 2, 'B': 3, 'C': 3, 'M': 3, 'P': 3, 'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4, 'K': 5, 'J': 8, 'X': 8, 'Q': 10, 'Z': 10, '*':0}
-#print(len(bag))
-#print()
-drawhand(hand,bag)
-#hand.remove(hand[1])
-#print(hand)
-dictionary = dictwrite('dictionary.txt')
-dictionary2 = elimwords(hand,dictionary)
-#print(dictionary2)
-print(hand)
-print(len(dictionary2))
+[b1,w1,t1] = gstart()
+d1 = dictwrite("dictionary.txt")
+addword(w1,"SMART",[7,7],"a",d1)
+#printwords(w1)
+#d2 = fixdict("dictionary.txt")
+temp = []
+c=0
+for i in range(15):
+    for j in range(15):
+        if w1[i][j] == 0 and c ==0:
+            1
+        else:
+            temp.append(w1[i][j])
+    temp2 = []
+    for j in range(len(temp)):
+        if temp[-j] == 0 and c ==0:
+            1
+        else:
+            temp.append(w1[i][j])
+    print("x")
+print(temp2)
