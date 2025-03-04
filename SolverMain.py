5f51abb91eea52827b161f6b1f8a55dffdc58a05
RyanYovichin <yovichin.ryan@gmail.com>
2025-03-04 12:39:12 -0800

    finished sorting rows in the words list to start looking through words

--- SolverMain.py
+++ SolverMain.py
@@ -7,22 +7,23 @@ hand = []
 bag = ["A","A","A","A","A","A","A","A","A","B","B","C","C","D","D","D","D","E","E","E","E","E","E","E","E","E","E","E","E","F","F","G","G","G","H","H","I","I","I","I","I","I","I","I","I","J","K","L","L","L","L","M","M","N","N","N","N","N","N","O","O","O","O","O","O","O","O","P","P","Q","R","R","R","R","R","R","S","S","S","S","T","T","T","T","T","T","U","U","U","U","V","V","W","W","X","Y","Y","Z","*","*"]
 [b1,w1,t1] = gstart()
 d1 = dictwrite("dictionary.txt")
-addword(w1,"SMART",[7,7],"a",d1)
+#addword(w1,"SMART",[7,7],"a",d1)
+w1[7][7] = "S"
+w1[12][7] = "A"
+w1[7][12] = "M"
 #printwords(w1)
 #d2 = fixdict("dictionary.txt")
-temp = []
-c=0
 for i in range(15):
+    temp=[]
+    temp2=[]
+    c=0
+    f=0
     for j in range(15):
-        if w1[i][j] == 0 and c ==0:
-            1
-        else:
+        if w1[i][j] != 0:
+            temp.append(f)
             temp.append(w1[i][j])
-    temp2 = []
-    for j in range(len(temp)):
-        if temp[-j] == 0 and c ==0:
-            1
+            f=0
         else:
-            temp.append(w1[i][j])
-    print("x")
-print(temp2)
+            f+=1
+    temp.append(f)
+    print(temp)
